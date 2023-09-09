888cbe5dd2dfe1e8fa5e97926ddbe8944bf2a7ef
Ozan YAPRAK <ozanerhanyaprak@icloud.com>
2023-09-09 15:51:49 +0300

    Update 09.09

---
+++ CideTarimKooperatif/ViewComponents/FooterViewComponent.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CideTarimKooperatif.ViewComponents
+{
+    public class FooterViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke() {  return View(); }
+    }
+}
---
+++ CideTarimKooperatif/ViewComponents/ScriptsViewComponent.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CideTarimKooperatif.ViewComponents
+{
+    public class ScriptsViewComponent:ViewComponent
+    {
+        public IViewComponentResult Invoke() {  return View(); }
+    }
+}
