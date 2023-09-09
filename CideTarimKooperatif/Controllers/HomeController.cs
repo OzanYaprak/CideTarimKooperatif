3240934229f362bc1c321d8dd74eb270cb61a1c5
Ozan YAPRAK <ozanerhanyaprak@icloud.com>
2023-09-09 14:37:52 +0300

    Update 09.09

--- CideTarimKooperatif/Controllers/HomeController.cs
+++ CideTarimKooperatif/Controllers/HomeController.cs
@@ -6,13 +6,6 @@ namespace CideTarimKooperatif.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             ViewBag.MenuIndex = 0;
@@ -20,15 +13,5 @@ namespace CideTarimKooperatif.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
